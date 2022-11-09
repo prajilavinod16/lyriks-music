33a25d45be4d48de2b05453583347bbeacdb0d44
prajilavinod16 <sreyadileep221@gmail.com>
2022-11-09 14:16:00 +0530

    music

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { genres } from "../../assets/constants";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "239b2ece02mshfbebfca94ead406p14efbcjsn2db7ec3f8525"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopcharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery, } = shazamCoreApi;
